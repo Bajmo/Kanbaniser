40532d400a8c238caa72987bbb2199c01611a0d6
Anas Mourad <anas.mourad.ma@gmail.com>
2023-11-20 10:55:38 +0100

    Adds linter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+const App = () => {
   return (
-    <>
-      test
-    </>
-  )
-}
+    <nav className="bg-red-900 w-screen h-48 flex">
+      <div className="text-white my-auto">
+        <h1 className="font-medium text-7xl">Homify</h1>
+      </div>
+    </nav>
+  );
+};
 
 export default App;
